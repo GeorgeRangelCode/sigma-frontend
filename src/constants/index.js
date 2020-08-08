d3e278c0ddde123555c8b0ef54a76c5f5a42f24a
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2020-08-08 15:23:27 -0500

    API

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,4 @@
-export const API =
+export const API_AWS =
   "https://sigma-studios.s3-us-west-2.amazonaws.com/test/colombia.json";
+export const API =
+  "https://raw.githubusercontent.com/GeorgeRangelCode/sigma-frontend/master/src/db/db.json";
